a86611105ec210457f6d32b194ebb819713b4d1b
Hs-the-dev <odioneghogho@gmail.com>
2025-02-20 15:53:26 +0100

    puahed height

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -7,7 +7,7 @@ function Main() {
     <div className="bg-[#292a2d] flex flex-col h-screen relative">
       <MainHeader />
       <div className="mx-auto h-full w-[95%] md:w-[80%] mb-2  ">
-        <div className="h-[80vh]">
+        <div className="h-[68vh]">
           <MainBody />
         </div>
         <div className="fixed  bottom-[2%] w-[95%] md:w-[80%]">
